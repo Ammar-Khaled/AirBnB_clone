97e9a9830a06522bd67a4a2175dd0c2b174b17c2
Fadi Asaad <firon1222@gmail.com>
2023-07-10 21:48:11 +0300

    Task 7: implement the console entry point prompt

--- console.py
+++ console.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""The console entry point of the AirBnB clone"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Class HBNBCommand"""
+
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    do_EOF = do_quit
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
