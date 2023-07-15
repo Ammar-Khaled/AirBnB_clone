67372d9faa2b912513aa51b32cb23d9fd3e55b74
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-15 11:50:23 +0000

    Implement unit test for City class

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Unittest for the City class."""
+
+
+from unittest import TestCase
+from models.city import City
+from datetime import datetime
+
+
+class TestBaseModel(TestCase):
+    """Test cases for City instances."""
+
+    def test_class_existence(self):
+        """Test is class exists."""
+        city = City()
+        self.assertEqual(str(city.__class__), "<class 'models.city.City'>")
+
+    def test_User_inheritance_from_BaseModel(self):
+        """Test if City class inherits from BaseModel class."""
+        city = City()
+        self.assertIsInstance(city, City)
+
+    def test_User_attributes(self):
+        """Test attributes of the City class."""
+        city = City()
+        self.assertTrue(hasattr(city, 'id'))
+        self.assertTrue(hasattr(city, 'created_at'))
+        self.assertTrue(hasattr(city, 'updated_at'))
+        self.assertTrue(hasattr(city, 'state_id'))
+        self.assertTrue(hasattr(city, 'name'))
+
+    def test_attr_types(self):
+        """Test types of City attributes."""
+        city = City()
+        self.assertIsInstance(city.id, str)
+        self.assertIsInstance(city.name, str)
+        self.assertIsInstance(city.created_at, datetime)
+        self.assertIsInstance(city.updated_at, datetime)
+        self.assertIsInstance(city.state_id, str)
