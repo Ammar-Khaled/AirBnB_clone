6740025fbf56ff8f70421fe52675f06faf5928e1
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-18 12:59:40 -0700

    Add test cases for console.

--- tests/test_console.py
+++ tests/test_console.py
@@ -328,3 +328,52 @@ Quit command to exit the program
         self.assertTrue(hasattr(obj, 'value'))
         self.assertIsInstance(getattr(obj, 'value'), str)
         self.assertEqual(getattr(obj, 'value'), '24')
+
+        def test_EOF(self):
+        """Test quit when EOF."""
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("EOF")
+
+        def test_empty_line(self):
+        """Test empty line."""
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("")
+                self.assertEqual(f.getvalue(), "")
+
+        def test_Review_all(self):
+        """Test Review.all()"""
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("Review.all()")
+                self.assertEqual(f.getvalue(), "")
+
+        def test_User_all(self):
+        """Test User.all()"""
+            with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("User.all()")
+            self.assertEqual(f.getvalue(), "")
+
+        def test_State_all(self):
+        """Test State.all()"""
+        with patch('sys.stdout', new=StringIO()) as f:
+        HBNBCommand().onecmd("State.all()")
+        self.assertEqual(f.getvalue(), "")
+
+        def test_City_all(self):
+        """Test City.all()"""
+            with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("City.all()")
+            self.assertEqual(f.getvalue(), "")
+
+
+        def test_Amenity_all(self):
+        """Test Amenity.all()"""
+            with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("Amenity.all()")
+            self.assertEqual(f.getvalue(), "")
+
+        def test_Place_all(self):
+        """Test Place.all()"""
+            with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("Place.all()")
+            self.assertEqual(f.getvalue(), "")
+
